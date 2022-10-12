25cbac15e2df0c2581dc475cc501ca97d8c9bff5
Frederik Petersen <fred.p@live.dk>
2022-10-12 15:04:48 +0200

    Import client from exercise 5

---
+++ client/client.go
@@ -0,0 +1,60 @@
+package main
+
+import (
+	"context"
+	"flag"
+	"fmt"
+	"log"
+	"time"
+
+	gRPC "github.com/ChrBank/DISYSExercise5/proto"
+	"google.golang.org/grpc"
+	"google.golang.org/grpc/credentials/insecure"
+)
+
+var clientsName = flag.String("name", "default", "Senders name")
+var serverPort = flag.String("server", "5400", "Tcp server")
+
+var server gRPC.TimeSyncClient  //the server
+var ServerConn *grpc.ClientConn //the server connection
+
+func main() {
+
+	fmt.Println("Please insert name:")
+	fmt.Scanf("%s", clientsName)
+
+	flag.Parse()
+
+	fmt.Println("--- CLIENT APP ---")
+
+	fmt.Println("--- join Server ---")
+
+	ConnectToServer()
+	defer ServerConn.Close()
+
+	parseInput()
+}
+
+func parseInput() {
+	server.Ping(context.Background(), &gRPC.Ack{ClientName: *clientsName})
+}
+
+func ConnectToServer() {
+	var opts []grpc.DialOption
+	opts = append(opts, grpc.WithBlock(), grpc.WithTransportCredentials(insecure.NewCredentials()))
+
+	//use context for timeout on the connection
+	timeContext, cancel := context.WithTimeout(context.Background(), time.Second)
+	defer cancel() //cancel the connection when we are done
+
+	log.Printf("client %s: Attempts to dial on port %s\n", *clientsName, *serverPort)
+	conn, err := grpc.DialContext(timeContext, fmt.Sprintf(":%s", *serverPort), opts...)
+	if err != nil {
+		log.Printf("Fail to Dial : %v", err)
+		return
+	}
+
+	server = gRPC.NewTimeSyncClient(conn)
+	ServerConn = conn
+	log.Println("the connection is: ", conn.GetState().String())
+}
