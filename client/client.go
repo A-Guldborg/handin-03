56307df5b9bdbcbbd738cff645c2e5c6d58eadac
Frederik Petersen <fred.p@live.dk>
2022-10-21 14:29:23 +0200

    shit works :tada:

--- client/client.go
+++ client/client.go
@@ -5,18 +5,16 @@ import (
 	"context"
 	"flag"
 	"fmt"
+	"io"
 	"log"
 	"os"
-	"strings"
-	"time"
 
 	gRPC "github.com/A-Guldborg/handin-03/proto"
 	"google.golang.org/grpc"
-	"google.golang.org/grpc/credentials/insecure"
 )
 
 var clientsName = flag.String("name", "default", "Senders name")
-var serverPort = flag.String("server", "5400", "Tcp server")
+var serverPort = flag.String("server", ":5400", "Tcp server")
 var LamportTimeStamp int64 = 1
 
 var server gRPC.ChittyChatClient //the server
@@ -24,68 +22,145 @@ var ServerConn *grpc.ClientConn  //the server connection
 
 func main() {
 
+	fmt.Println("--- CLIENT APP ---")
 	fmt.Println("Please insert name:")
 	fmt.Scanf("%s", clientsName)
 
 	flag.Parse()
 
-	fmt.Println("--- CLIENT APP ---")
-
 	fmt.Println("--- join Server ---")
 
 	ConnectToServer()
+
+	fmt.Println("Connected to server")
 	defer ServerConn.Close()
 
-	JoinServer()
+	go JoinServer()
 
 	parseInput()
 }
 
 func JoinServer() {
-	server.Join(context.Background(), &gRPC.BasePacket{ClientName: *clientsName, LamportTimeStamp: LamportTimeStamp})
+	log.Println("Hello world")
+	pkt := gRPC.BasePacket{
+		LamportTimeStamp: LamportTimeStamp,
+		ClientName:       *clientsName,
+	}
+	client, err := server.Join(context.Background(), &pkt)
+	if err != nil {
+		log.Println("Error: " + err.Error())
+	}
+
+	incuming := make(chan struct{})
+
+	go func() {
+		for {
+			// in, err := client.Recv()
+
+			in := gRPC.Content{}
+
+			err := client.RecvMsg(&in)
+			if err == io.EOF {
+				close(incuming)
+				return
+			}
+			if err != nil {
+				log.Fatalf("Failed to receive message from channel joining. \nErr: %v", err)
+			}
+
+			if *clientsName != in.BasePacket.ClientName {
+				fmt.Printf("MESSAGE: (%v) -> %v \n", in.BasePacket.ClientName, in.MessageBody)
+			}
+		}
+	}()
+
+	<-incuming
+
+	// confirmation, _ := client.Recv()
+	// log.Println(confirmation.MessageBody)
+}
+
+func sendMessage(message string) {
+	stream, err := server.Message(context.Background())
+
+	if err != nil {
+		log.Printf("Cannot send message: error: %v", err)
+	}
+	msg := gRPC.Content{
+		BasePacket: &gRPC.BasePacket{
+			ClientName:       *clientsName,
+			LamportTimeStamp: 1 + 1,
+		},
+		MessageBody: message,
+	}
+	stream.Send(&msg)
+
+	ack, err := stream.CloseAndRecv()
+	if err != nil {
+		fmt.Printf("Error while sending: %s \n", err)
+	} else {
+		fmt.Printf("Message sent: %v \n", ack)
+	}
 }
 
 func parseInput() {
 	reader := bufio.NewReader(os.Stdin)
+	defer ServerConn.Close()
 	for {
-		fmt.Print("-> ")
 
+		fmt.Print(*clientsName + " -> ")
 		input, err := reader.ReadString('\n')
+
+		if input == "leave" {
+			return
+		}
+
+		go sendMessage(input)
+
 		if err != nil {
 			log.Fatal(err)
 		}
-		input = strings.TrimSpace(input)
-
-		if !conReady(server) {
-			log.Printf("Client %s: Something was wrong with connection :(", *clientsName)
-			continue
-		}
-
 		// Send to server
-		server.Message(context.Background(), &gRPC.Content{ClientName: *clientsName, LamportTimeStamp: LamportTimeStamp, MessageBody: input})
+		// server.Message(context.Background(), &gRPC.Content{BasePacket: &gRPC.BasePacket{ClientName: *clientsName, LamportTimeStamp: LamportTimeStamp}, MessageBody: input})
+		LamportTimeStamp++
 	}
 }
+
+// func ConnectToServer() {
+// 	var opts []grpc.DialOption
+// 	opts = append(opts, grpc.WithBlock(), grpc.WithInsecure())
+
+// 	//use context for timeout on the connection
+// 	timeContext, cancel := context.WithTimeout(context.Background(), time.Second)
+// 	defer cancel() //cancel the connection when we are done
+
+// 	log.Printf("client %s: Attempts to dial on port %s\n", *clientsName, *serverPort)
+// 	conn, err := grpc.DialContext(timeContext, fmt.Sprintf(":%s", *serverPort), opts...)
+// 	if err != nil {
+// 		log.Printf("Fail to Dial : %v", err)
+// 		return
+// 	}
+
+// 	server = gRPC.NewChittyChatClient(conn)
+// 	ServerConn = conn
+// 	log.Println("the connection is: ", conn.GetState().String())
+// }
 
 func ConnectToServer() {
 	var opts []grpc.DialOption
-	opts = append(opts, grpc.WithBlock(), grpc.WithTransportCredentials(insecure.NewCredentials()))
-
-	//use context for timeout on the connection
-	timeContext, cancel := context.WithTimeout(context.Background(), time.Second)
-	defer cancel() //cancel the connection when we are done
-
-	log.Printf("client %s: Attempts to dial on port %s\n", *clientsName, *serverPort)
-	conn, err := grpc.DialContext(timeContext, fmt.Sprintf(":%s", *serverPort), opts...)
+	opts = append(opts, grpc.WithBlock(), grpc.WithInsecure())
+	fmt.Printf("Dialing on %s \n", *serverPort)
+	conn, err := grpc.Dial(*serverPort, opts...)
+	fmt.Println("Dialed")
 	if err != nil {
-		log.Printf("Fail to Dial : %v", err)
-		return
+		log.Fatalf("Fail to dail: %v", err)
 	}
+	fmt.Println("Didn't fail to dial")
+
+	fmt.Println(conn.GetState().String())
 
 	server = gRPC.NewChittyChatClient(conn)
-	ServerConn = conn
-	log.Println("the connection is: ", conn.GetState().String())
-}
-
-func conReady(s gRPC.ChittyChatClient) bool {
-	return ServerConn.GetState().String() == "READY"
+	if err != nil {
+		log.Fatalf("Fail to dail: %v", err)
+	}
 }
--- server/server.go
+++ server/server.go
@@ -3,6 +3,8 @@ package main
 import (
 	"context"
 	"flag"
+	"fmt"
+	"io"
 	"log"
 	"net"
 
@@ -13,31 +15,51 @@ import (
 type Server struct {
 	gRPC.UnimplementedChittyChatServer
 
-	port string
+	channels []chan *gRPC.Content
+	port     string
 }
 
 var port = flag.String("port", "5400", "Server port")
 
-func main() {
-	listener, _ := net.Listen("tcp", "localhost:5400")
-	grpcServer := grpc.NewServer()
-	server := &Server{
-		port: *port,
+func newServer() *Server {
+	s := &Server{
+		port:     "5400",
+		channels: make([]chan *gRPC.Content, 0),
 	}
+	fmt.Println(s)
+	return s
+}
 
-	gRPC.RegisterChittyChatServer(grpcServer, server)
+func main() {
+	// Start server up to be listened to
+	listener, err := net.Listen("tcp", "localhost:"+*port)
+	log.Printf("Listening on port %s \n", *port)
+	if err != nil {
+		log.Fatalf("Failed to listen: %v", err)
+	}
+	grpcServer := grpc.NewServer()
+
+	gRPC.RegisterChittyChatServer(grpcServer, newServer())
 	grpcServer.Serve(listener)
 }
 
-func (s *Server) Join(ctx context.Context, basepacket *gRPC.BasePacket) (*gRPC.BasePacket, error) {
-	var clientName = basepacket.ClientName
-	var LamportTimeStamp = basepacket.LamportTimeStamp
+func (s *Server) Join(basepacket *gRPC.BasePacket, stream gRPC.ChittyChat_JoinServer) error {
+	// Client joins
+	newChannel := make(chan *gRPC.Content)
+	s.channels = append(s.channels, newChannel)
 
-	// Broadcast stuff
+	log.Println("Server served")
 
-	log.Printf("%s joined the chat at L(%d)", clientName, LamportTimeStamp)
-
-	return basepacket, nil
+	// doing this never closes the stream
+	for {
+		select {
+		case <-stream.Context().Done():
+			return nil
+		case msg := <-newChannel:
+			log.Printf("GO ROUTINE (got message): %v \n", msg)
+			stream.Send(msg)
+		}
+	}
 }
 
 func (s *Server) Leave(ctx context.Context, basepacket *gRPC.BasePacket) (*gRPC.BasePacket, error) {
@@ -51,19 +73,51 @@ func (s *Server) Leave(ctx context.Context, basepacket *gRPC.BasePacket) (*gRPC.
 	return basepacket, nil
 }
 
-func (s *Server) Message(ctx context.Context, message *gRPC.Content) (*gRPC.BasePacket, error) {
-	var clientName = message.ClientName
-	var LamportTimeStamp = message.LamportTimeStamp
-	var body = message.MessageBody
+// func (s *Server) Message(ctx context.Context, message *gRPC.Content) (*gRPC.BasePacket, error) {
+// 	var clientName = message.BasePacket.ClientName
+// 	var LamportTimeStamp = message.BasePacket.LamportTimeStamp
+// 	var body = message.MessageBody
 
-	// Broadcast stuff
+// 	log.Printf("%s joined the chat at L(%d)", clientName, LamportTimeStamp)
 
-	log.Printf("%s L(%d): %s", clientName, LamportTimeStamp, body)
+// 	// Broadcast stuff
 
-	var basepacket = gRPC.BasePacket{
-		ClientName: clientName,
-		LamportTimeStamp: LamportTimeStamp,
+// 	//
+
+// 	log.Printf("%s L(%d): %s", clientName, LamportTimeStamp, body)
+
+// 	var basepacket = gRPC.BasePacket{
+// 		ClientName:       clientName,
+// 		LamportTimeStamp: LamportTimeStamp,
+// 	}
+
+// 	return &basepacket, nil
+// }
+
+func (s *Server) Message(stream gRPC.ChittyChat_MessageServer) error {
+	// Server connection starts
+	log.Println("Server connection started")
+
+	log.Println("Trying to receive")
+	in, err := stream.Recv()
+	log.Println("message recieved?")
+	if err == io.EOF {
+		return nil // Client stops connection
+	} else if err != nil {
+		return err // Error happened
 	}
-	
-	return &basepacket, nil
+
+	log.Printf("%d - %s -> %s ", in.BasePacket.LamportTimeStamp, in.BasePacket.ClientName, in.MessageBody) // log message
+
+	ack := gRPC.Ack{StatusCode: 200}
+	stream.SendAndClose(&ack)
+
+	go func() {
+		streams := s.channels
+		for _, msgChan := range streams {
+			msgChan <- in
+		}
+	}()
+
+	return nil
 }
